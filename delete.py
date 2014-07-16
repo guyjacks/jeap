6ed5d591825a2b5fb673abbcef2647838e8ffe03
Guy Jacks <guy.jacks@gmail.com>
2014-07-16 11:30:12 -0400

    added gitignore file

--- delete.py
+++
@@ -1,9 +0,0 @@
-class Jeap(object):
-    def __init__(self):
-        events = Events()
-        tokens = Tokens()
-        parser_state_machine = ParserStateMachine(events, tokens)
-        parser = Parser(events, tokens, parser_state_machine)
-        compiler_state_machine = CompilerStateMachine(tokens)
-        ast = Nodes()
-        compiler = Compiler(tokens, parser, compiler_state_machine, ast)
