d571b866aa4951209243296e808358440e091355
Guy Jacks <guy.jacks@gmail.com>
2014-08-12 20:11:45 -0400

    Tested evaluation of And op and Or op.

--- test/nodes.AndOperatorNode/test_evaluate.py
+++ test/nodes.AndOperatorNode/test_evaluate.py
@@ -6,13 +6,16 @@ def test_base(node_tree):
     false_node = nodes.ExpressionLiteralNode(False, node_tree)
     and_node = nodes.AndOperatorNode(node_tree)
 
+    # True and False
     and_node.left = true_node
     and_node.right = false_node
     assert and_node.evaluate() == False
 
+    # True and True
     and_node.right = true_node
     assert and_node.evaluate() == True
 
+    # False and False
     and_node.left = false_node
     and_node.right = false_node
     assert and_node.evaluate() == False
--- test/nodes.OrOperatorNode/test_evaluate.py
+++ test/nodes.OrOperatorNode/test_evaluate.py
@@ -5,16 +5,20 @@ def test_base(node_tree):
     false_node = nodes.ExpressionLiteralNode(False, node_tree)
     or_node = nodes.OrOperatorNode(node_tree)
 
+    # True or False
     or_node.left = true_node
     or_node.right = false_node
     assert or_node.evaluate() == True
 
+    # True or True
     or_node.right = true_node
     assert or_node.evaluate() == True
 
+    # False or True
     or_node.left = false_node
     assert or_node.evaluate() == True
 
+    # False or False
     or_node.left = false_node
     or_node.right = false_node
     assert or_node.evaluate() == False
